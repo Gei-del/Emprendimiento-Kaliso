f2561077c176548564409913c642ea83265b7ca3
Lorena <130115448+Gei-del@users.noreply.github.com>
2026-09-03 06:45:34 -0500

    Conecta KASOLI con Supabase

--- Downloads/kaliso/js/config.js
+++ Downloads/kaliso/js/config.js
@@ -2,7 +2,7 @@ window.KASOLI_CONFIG={
   businessName:"KASOLI",
   instagram:"https://www.instagram.com/kaliso35",
   advisors:[{name:"Lorena",phone:"573174070511"},{name:"Karen",phone:"573233067322"}],
-  supabaseUrl:"",
-  supabaseAnonKey:"",
-  adminEmails:["gpontonca@gmail.com"]
+  supabaseUrl:"https://vruvsvbgqfcotmrmcaqi.supabase.co",
+  supabaseAnonKey:"sb_publishable_1ytEp60jZrw-SQ-sESWyvw_4fwYZGSr",
+  adminEmails:["pontongeidy@gmail.com","karenmorales12386@gmail.com"]
 };
