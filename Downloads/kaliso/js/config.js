9fd82308c60294e1ace3a55f2d549c0bc2a116eb
Lorena <130115448+Gei-del@users.noreply.github.com>
2026-09-03 09:26:31 -0500

    Actualiza configuración de Instagram KASOLI

--- Downloads/kaliso/js/config.js
+++ Downloads/kaliso/js/config.js
@@ -1,6 +1,6 @@
 window.KASOLI_CONFIG={
   businessName:"KASOLI",
-  instagram:"https://www.instagram.com/kaliso35",
+  instagram:"https://www.instagram.com/kasoli_35",
   advisors:[{name:"Lorena",phone:"573174070511"},{name:"Karen",phone:"573233067322"}],
   supabaseUrl:"https://vruvsvbgqfcotmrmcaqi.supabase.co",
   supabaseAnonKey:"sb_publishable_1ytEp60jZrw-SQ-sESWyvw_4fwYZGSr",
